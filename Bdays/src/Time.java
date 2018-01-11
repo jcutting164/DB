d430ffbfc7b4f8d7e274b7ee7ff97676d4ead4d7
Josh Cutting <jcutting164@gmail.com>
2018-01-11 15:56:35 -0500

    Update Time.java

--- Bdays/src/Time.java
+++ Bdays/src/Time.java
@@ -20,4 +20,14 @@ public class Time {
 		String actDate = time[2] + ":" + time[1] +"."+ time[0]+"";
 		return actDate;
 	}
+	
+	public String getShortDate(){
+		String timeStamp = new SimpleDateFormat("yyyy/MM/dd:HH|mm|ss").format(Calendar.getInstance().getTime());
+		String[] date = timeStamp.split(":");
+		String temp = date[0];
+		date = temp.split("/");
+		String shortDate = date[2] + "/" + date[1];
+		System.out.println("The current date is: "+shortDate);
+		return shortDate;
+	}
 }
